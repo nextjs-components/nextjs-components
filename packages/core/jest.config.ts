6eebeabe3896102d57eca39d0dd6e24acb5e691e
Kevin Wang <kwangsan@gmail.com>
2022-01-11 23:23:00 -0500

    feat: test setup (#29)
    * chore: test setup
    
    * chore: coverage options
    
    * feat: tests in CI
    
    * chore: script updates
    
    * chore: more script updates
    
    * chore: use `ts-jext/utils`

---
+++ packages/core/jest.config.ts
@@ -0,0 +1,21 @@
+// jest.config.ts
+import type { Config } from "@jest/types";
+
+/**
+ * @see https://jestjs.io/docs/configuration
+ */
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    testEnvironment: "jsdom", // mock document & window
+    moduleDirectories: ["src", "node_modules"],
+    testPathIgnorePatterns: ["dist", "node_modules"],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    transform: {
+      "^.+\\.tsx?$": ["babel-jest", { presets: ["next/babel"] }],
+      ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
+    },
+    // skip coverage reporting
+    coveragePathIgnorePatterns: ["src/icons", "index.tsx"],
+    collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx"],
+  };
+};
