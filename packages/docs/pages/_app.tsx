0812658eaf53818e0b327f5dc2b7628c68d0ecd5
Kevin Wang <kwangsan@gmail.com>
2022-01-15 00:40:07 -0500

    chore(docs): darkmode script

--- packages/docs/pages/_app.tsx
+++ packages/docs/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "nextjs-components/dist/styles/globals.css";
 
+import Head from "next/head";
 import {
   ThemeContextProvider,
   ToastsProvider,
@@ -8,12 +9,22 @@ import {
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeContextProvider>
-      <ToastsProvider>
-        <Component {...pageProps} />
-        <ToastArea />
-      </ToastsProvider>
-    </ThemeContextProvider>
+    <>
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `!function(){let e,t;try{e=localStorage.getItem("mode")}catch(e){}var d=window.matchMedia("(prefers-color-scheme: dark)");"boolean"==typeof d.matches&&(t=d.matches),null===e&&t&&document.documentElement.classList.add("dark-theme"),"dark"===e&&document.documentElement.classList.add("dark-theme"),"system"===e&&t&&document.documentElement.classList.add("dark-theme"),window.__mode=e||"system"}();`,
+          }}
+        />
+      </Head>
+      <ThemeContextProvider>
+        <ToastsProvider>
+          <Component {...pageProps} />
+          <ToastArea />
+        </ToastsProvider>
+      </ThemeContextProvider>
+    </>
   );
 }
 
