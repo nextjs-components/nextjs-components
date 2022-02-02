7031aa03d222d5f2665d722647b4c885b9d08dd1
Kevin Wang <kwangsan@gmail.com>
2022-02-02 08:21:15 -0500

    chore: simplify ThemeContext

--- packages/core/src/contexts/ThemeContext/ThemeContext.ts
+++ packages/core/src/contexts/ThemeContext/ThemeContext.ts
@@ -1,14 +1,9 @@
 import { createContext } from "react";
 
-export enum Modes {
-  LIGHT = "light",
-  DARK = "dark",
-  SYSTEM = "system",
-}
 export interface IThemeContext {
-  selectTheme: (val: Modes | "light" | "dark" | "system" | string) => void;
+  selectTheme: (val: "light" | "dark" | "system" | string) => void;
   isDarkMode: null | boolean;
-  mode: Modes | "light" | "dark" | "system";
+  mode: "light" | "dark" | "system";
 }
 
 export const ThemeContext = createContext<IThemeContext>({
