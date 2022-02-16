0e94a03111b51bf881efe4391a1be230821560aa
Kevin Wang <kwangsan@gmail.com>
2022-02-16 09:51:47 -0500

    chore: leave `todo` test

---
+++ packages/core/src/components/Modal/Modal.test.tsx
@@ -0,0 +1,31 @@
+import { render, fireEvent, act } from "@testing-library/react";
+
+import Modal from "./";
+
+const Component = ({ active }: { active: boolean }) => {
+  return (
+    <Modal.Modal active={active}>
+      <Modal.Body>
+        <Modal.Header>
+          <Modal.Title>Modal</Modal.Title>
+          <Modal.Subtitle>This is a modal.</Modal.Subtitle>
+        </Modal.Header>
+      </Modal.Body>
+
+      <Modal.Actions>
+        <Modal.Action onClick={close}>Cancel</Modal.Action>
+
+        <Modal.Action onClick={close}>Submit</Modal.Action>
+      </Modal.Actions>
+    </Modal.Modal>
+  );
+};
+
+describe("Modal", () => {
+  describe("scroll lock", () => {
+    // GIVEN
+    it.todo("should be enabled when the modal is active");
+    // GIVEN
+    it.todo("should be disabled when the modal is not active");
+  });
+});
