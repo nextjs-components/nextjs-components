40de9658472e16b429c47d71a7358a95351123bb
Kevin Wang <kwangsan@gmail.com>
2022-04-17 07:49:38 -0400

    fix(UL/OL/LI): optional classname

--- packages/core/src/components/Text/OL.tsx
+++ packages/core/src/components/Text/OL.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import styles from "./list.module.css";
 
-const OL: React.FC<{ classname: string }> = ({ children, classname }) => {
+const OL: React.FC<{ classname?: string }> = ({ children, classname }) => {
   return <ol className={clsx(styles.ol, classname)}>{children}</ol>;
 };
 
