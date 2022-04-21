146e5e2cf29feca8194b60722f0d648d85c81386
Kevin Wang <kwangsan@gmail.com>
2022-04-21 09:46:15 -0400

    fix(Icons.tag): invalid jsx property

--- packages/core/src/icons/Tag.tsx
+++ packages/core/src/icons/Tag.tsx
@@ -11,11 +11,11 @@ const Tag = ({ color = "currentColor", size }: Props) => {
       width={size || iconSize.size}
       height={size || iconSize.size}
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       fill="none"
-      shape-rendering="geometricPrecision"
+      shapeRendering="geometricPrecision"
       style={{ color }}
     >
       <path d="M20.59 13.41l-7.17 7.17a2 2 0 01-2.83 0L2 12V2h10l8.59 8.59a2 2 0 010 2.82z" />
