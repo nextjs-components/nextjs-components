60c005cb91c6abfeea6c4598611599f29be0dcf7
Kevin Wang <kwangsan@gmail.com>
2022-08-11 08:59:37 -0400

    chore: ModeSelect `useFocusRing`

--- packages/core/src/components/Select/ModeSelect.tsx
+++ packages/core/src/components/Select/ModeSelect.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import clsx from "clsx";
-import { FocusRing } from "@react-aria/focus";
+import { useFocusRing } from "@react-aria/focus";
 import { useId } from "@react-aria/utils";
 
 import { useTheme } from "../../contexts/ThemeContext";
@@ -14,6 +14,7 @@ import Label from "../Label/Label";
 import styles from "./Select.module.css";
 
 const ModeSelect = () => {
+  const { isFocusVisible, focusProps } = useFocusRing();
   const id = useId();
   const { selectTheme, mode } = useTheme();
   return (
@@ -25,21 +26,24 @@ const ModeSelect = () => {
             {mode === "light" && <Sun />}
             {mode === "system" && <DisplayIcon />}
           </span>
-          <FocusRing focusRingClass={"focus-visible"}>
-            <select
-              id={id}
-              className={clsx(styles.select, styles.small)}
-              aria-label="Change color theme"
-              onChange={(e) => {
-                selectTheme(e.target.value);
-              }}
-              value={mode}
-            >
-              <option value="system">System</option>
-              <option value="dark">Dark</option>
-              <option value="light">Light</option>
-            </select>
-          </FocusRing>
+
+          <select
+            {...focusProps}
+            id={id}
+            className={clsx(styles.select, styles.small, {
+              ["focus-visible"]: isFocusVisible,
+            })}
+            aria-label="Change color theme"
+            onChange={(e) => {
+              selectTheme(e.target.value);
+            }}
+            value={mode}
+          >
+            <option value="system">System</option>
+            <option value="dark">Dark</option>
+            <option value="light">Light</option>
+          </select>
+
           <span className={styles.suffix}>
             <ChevronUpDown />
           </span>
