b474e36e79cbe4a3e85752394a9c05cd6a97541d
Kevin Wang <kwangsan@gmail.com>
2022-10-01 00:55:06 -0400

    chore(docs): autolink mdx headings

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -2,25 +2,31 @@
 // a `.mjs` file. https://github.com/vercel/next.js/issues/9607#issuecomment-944156493
 //
 // if using `.js`, you may see "SyntaxError: Cannot use import statement outside a module"
-import withMDX from '@next/mdx';
-import withPlugins from 'next-compose-plugins';
-import withTM from 'next-transpile-modules';
-import rehypeSlug from 'rehype-slug';
-import remarkGfm from 'remark-gfm';
+import withMDX from "@next/mdx";
+import withPlugins from "next-compose-plugins";
+import withTM from "next-transpile-modules";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 const plugins = [
   withMDX({
     extension: /\.mdx?$/,
     options: {
-      // remarkGfm is required to process syntax
-      // like tables
+      // remarkGfm is required to process syntax like tables
       remarkPlugins: [remarkGfm],
-      rehypePlugins: [rehypeSlug],
+      rehypePlugins: [
+        rehypeSlug, // inject `id` into headings
+        [
+          rehypeAutolinkHeadings,
+          { behavior: "wrap", test: ["h2", "h3", "h4", "h5", "h6"] },
+        ],
+      ],
       // this is here since we're using `MDXProvider`
-      providerImportSource: '@mdx-js/react',
+      providerImportSource: "@mdx-js/react",
     },
   }),
-  withTM(['nextjs-components', '../core']),
+  withTM(["nextjs-components", "../core"]),
 ];
 
 /**
@@ -28,20 +34,7 @@ const plugins = [
  */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['mdx', 'jsx', 'js', 'tsx', 'ts'],
-  webpack: (config, options) => {
-    // if (options.isServer) {
-    //   config.externals = ['react', ...config.externals];
-    // }
-
-    // // if using next.config.mjs, ReferenceError: __dirname is not defined
-    // // - https://stackoverflow.com/a/66651120/9823455
-    // const __dirname = new URL('.', import.meta.url).pathname;
-    // // Point to 1 copy of react
-    // config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
-
-    return config;
-  },
+  pageExtensions: ["mdx", "tsx", "ts"],
 };
 
 export default withPlugins(plugins, nextConfig);
