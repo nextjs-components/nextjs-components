5f94716db03033777ddd0bc257e9097de385eb69
Kevin Wang <kwangsan@gmail.com>
2023-02-19 12:04:29 -0500

    add next-themes

--- packages/docs/app/layout.tsx
+++ packages/docs/app/layout.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
 import "nextjs-components/src/styles/globals.css";
 
 // Must have html and body tags or else:
 // Uncaught Error: invariant expected app router to be mounted
-export default async function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          enableSystem
+          value={{
+            dark: "dark-theme", // specific html tag class used by all components' css
+            system: "system", // arbitrary
+            light: "light-theme", // arbitrary
+          }}
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
