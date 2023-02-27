71ae657bf227e2f8d57916474f75acf6f4d93792
Kevin Wang <kwangsan@gmail.com>
2023-02-27 08:51:25 -0500

    progress

--- packages/core/src/components/tabs/tabs.tsx
+++ packages/core/src/components/tabs/tabs.tsx
@@ -1,9 +1,67 @@
+import clsx from "clsx";
 import React from "react";
+import { useFocus } from "react-aria";
 
-import styles from "./Tabs.module.css";
+import { Text } from "../Text";
+import styles from "./tabs.module.css";
 
-const Tabs: React.ComponentType<React.PropsWithChildren> = ({ children }) => {
-  return <div className={styles.tabs}>{children}</div>;
+interface TabsProps extends React.PropsWithChildren {
+  tabs: { title: string; value: string; icon?: JSX.Element }[];
+  selected?: string;
+  setSelected?: (value: string) => void;
+}
+const Tabs: React.ComponentType<TabsProps> = ({
+  children,
+  tabs,
+  selected,
+  setSelected,
+}) => {
+  return (
+    <div className={clsx("geist-no-scrollbar", styles.tabs)} data-geist-tabs="">
+      {tabs.map((tab) => {
+        return (
+          <Tab
+            key={tab.value}
+            title={tab.title}
+            value={tab.value}
+            selected={selected}
+            setSelected={setSelected}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Tabs;
+
+const Tab = ({ title, value, selected, setSelected }) => {
+  let [focused, setFocused] = React.useState(false);
+  let { focusProps } = useFocus({
+    onFocusChange: setFocused,
+  });
+  return (
+    <div
+      {...focusProps}
+      className={styles.tabContainer}
+      data-geist-tab=""
+      data-focus-visible-added={focused}
+      role="button"
+      tabIndex={0}
+      onClick={() => setSelected?.(value)}
+      onKeyDown={(e) => {
+        if (e.key === " ") {
+          setSelected?.(value);
+        }
+      }}
+    >
+      <div
+        className={clsx(styles.tab, {
+          [styles.activeTab]: selected == value,
+        })}
+      >
+        <Text>{title}</Text>
+      </div>
+    </div>
+  );
+};
