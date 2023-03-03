bc0a53e88e3e586e21af507592a366077d7ef4b5
Kevin Wang <kwangsan@gmail.com>
2023-03-03 08:06:50 -0500

    core(SearchInput): update with clear button

--- packages/core/src/components/Input/SearchInput.tsx
+++ packages/core/src/components/Input/SearchInput.tsx
@@ -1,26 +1,62 @@
-import clsx from "clsx";
 import React from "react";
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { forwardRef, useRef } from "react";
 
 import Search from "../../icons/search";
-import styles from "./SearchInput.module.css";
+import { mergeRefs } from "../../utils/merge-refs";
+import { Spinner } from "../Spinner";
+import { Props as BaseProps, default as Input } from "./Input";
+import { ClearButton } from "./clear-button";
 
 interface Props
-  extends DetailedHTMLProps<
-    InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  > {}
-const SearchInput: React.ComponentType<Props> = ({ className, ...props }) => {
-  return (
-    <div className={styles.wrapper}>
-      <span className={styles.icon}>
-        <Search />
-      </span>
-      <div className={styles.input_wrapper}>
-        <input className={clsx(className, styles.input)} {...props} />
-      </div>
-    </div>
-  );
-};
+  extends Omit<
+    BaseProps,
+    | "prefix"
+    | "prefixStyling"
+    | "prefixContainer"
+    | "suffix"
+    | "suffixStyling"
+    | "suffixContainer"
+  > {
+  loading?: boolean;
+}
+
+const SearchInput = forwardRef<HTMLInputElement, Props>(
+  ({ loading, ...props }, externalRef) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    return (
+      <Input
+        {...props}
+        ref={mergeRefs([externalRef, inputRef])}
+        aria-label={props["aria-label"] || "Search"}
+        typeName="search"
+        prefix={
+          loading ? (
+            <Spinner size={{ small: 16, large: 24 }[props.size]} />
+          ) : (
+            <Search />
+          )
+        }
+        prefixStyling={false}
+        suffix={
+          props.value || inputRef.current?.value ? (
+            <ClearButton
+              onClick={() => {
+                if (inputRef.current) {
+                  inputRef.current.value = "";
+                  inputRef.current.focus();
+                  props.onChange?.({
+                    target: { value: "" },
+                  } as any);
+                }
+              }}
+            />
+          ) : null
+        }
+        suffixContainer={false}
+        suffixStyling={false}
+      />
+    );
+  },
+);
 
 export default SearchInput;
