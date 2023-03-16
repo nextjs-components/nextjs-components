0207489e1ad1e24df80bbc4d4d287599341912e8
Kevin Wang <kwangsan@gmail.com>
2023-03-16 19:28:09 -0400

    docs: codeblock syntax highlighting

--- packages/docs/app/layout.tsx
+++ packages/docs/app/layout.tsx
@@ -3,6 +3,8 @@
 import { ThemeContextProvider } from "nextjs-components/src/contexts/ThemeContext";
 import "nextjs-components/src/styles/globals.css";
 
+import "./pretty-lights.css";
+
 // Must have html and body tags or else:
 // Uncaught Error: invariant expected app router to be mounted
 export default function RootLayout({ children }) {
