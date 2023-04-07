25933ce0f98d68ad585c58829fd6edfb16e328a8
Kevin Wang <kwangsan@gmail.com>
2023-04-07 10:09:07 -0400

    improve click

--- packages/core/src/components/calendar/calendar-cell.tsx
+++ packages/core/src/components/calendar/calendar-cell.tsx
@@ -1,14 +1,25 @@
-import { isSameDay, isSameMonth, isToday } from "@internationalized/date";
+import {
+  type CalendarDate,
+  isSameDay,
+  isSameMonth,
+  isToday,
+} from "@internationalized/date";
 import { useCalendarCell } from "@react-aria/calendar";
-import { useLocale } from "@react-aria/i18n";
 import { mergeProps } from "@react-aria/utils";
 import clsx from "clsx";
 import { useRef } from "react";
 import { useHover } from "react-aria";
+import { RangeCalendarState } from "react-stately";
 
 import styles from "./calendar.module.css";
 
-export function CalendarCell({ state, date }) {
+export function CalendarCell({
+  state,
+  date,
+}: {
+  state: RangeCalendarState;
+  date: CalendarDate;
+}) {
   let ref = useRef();
   let {
     cellProps,
@@ -19,9 +30,7 @@ export function CalendarCell({ state, date }) {
     formattedDate,
     isInvalid,
   } = useCalendarCell({ date }, state, ref);
-
-  let { locale } = useLocale();
-
+  state;
   let highlightedRange = "highlightedRange" in state && state.highlightedRange;
 
   let isFirstInRange =
@@ -30,9 +39,29 @@ export function CalendarCell({ state, date }) {
 
   let { hoverProps, isHovered } = useHover({});
 
+  let isTechnicallyValid = true;
+  if (state.maxValue) {
+    if (date > state.maxValue) {
+      isTechnicallyValid = false;
+    }
+  }
+  if (state.minValue) {
+    if (date < state.minValue) {
+      isTechnicallyValid = false;
+    }
+  }
+  if (isTechnicallyValid) {
+    cellProps["aria-disabled"] = false;
+    if (highlightedRange) {
+      if (date >= highlightedRange.start && date <= highlightedRange.end) {
+        cellProps["aria-selected"] = true;
+      }
+    }
+  }
+
   return (
     <td
-      {...cellProps}
+      {...cellProps} // role, aria-selected, aria-disabled
       className={clsx({
         [styles.firstInRange]: isFirstInRange,
         [styles.lastInRange]: isLastInRange,
@@ -51,8 +80,15 @@ export function CalendarCell({ state, date }) {
           ), // faded
           [styles.hovered]: isHovered, // thin ring
           [styles.selected]: isFirstInRange || isLastInRange, // White
-          [styles.disabled]: isDisabled, // faded; no hover ring
+          [styles.disabled]: !isTechnicallyValid && isDisabled, // faded; no hover ring
         })}
+        // TODO: improve onClick & onMouseOver
+        onClick={() => {
+          if (isTechnicallyValid && isDisabled) {
+            // state.setFocusedDate(null);
+            state.selectDate(date);
+          }
+        }}
       >
         {formattedDate}
       </span>
