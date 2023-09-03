804e322edeff2f49727ab2e1239be189661952d6
Kevin Wang <kwangsan@gmail.com>
2023-09-03 09:51:34 -0400

    docs: make page RSC first

--- packages/docs/app/design/(components)/[slug]/page.tsx
+++ packages/docs/app/design/(components)/[slug]/page.tsx
@@ -1,103 +1,24 @@
-"use client";
+import { type ResolvingMetadata } from "next";
+import { notFound } from "next/navigation";
+import { type PropsWithChildren } from "react";
 
-import Breadcrumbs from "./breadcrumbs.mdx";
-import Button from "./button.mdx";
-import Calendar from "./calendar.mdx";
-import Capacity from "./capacity.mdx";
-import Combobox from "./combobox.mdx";
-import Description from "./description.mdx";
-import Entity from "./entity.mdx";
-import Fieldset from "./fieldset.mdx";
-import FileTree from "./file-tree.mdx";
-import Input from "./input.mdx";
-import KeyboardInput from "./keyboard-input.mdx";
-import LoadingDots from "./loading-dots.mdx";
-import Menu from "./menu.mdx";
-import Modal from "./modal.mdx";
-import Note from "./note.mdx";
-import Radio from "./radio.mdx";
-import Select from "./select.mdx";
-import ShowMore from "./show-more.mdx";
-import Spinner from "./spinner.mdx";
-import Stack from "./stack.mdx";
-import StatusDot from "./status-dot.mdx";
-import Switch from "./switch.mdx";
-import Table from "./table.mdx";
-import Tabs from "./tabs.mdx";
-import Text from "./text.mdx";
-import Textarea from "./textarea.mdx";
-import Toggle from "./toggle.mdx";
-import Tooltip from "./tooltip.mdx";
+import Renderer from "./renderer";
 
-export default function Slug({ params: { slug } }) {
-  const Page = () => {
-    switch (slug) {
-      case "breadcrumbs":
-        return <Breadcrumbs />;
-      case "button":
-        return <Button />;
-      case "calendar":
-        return <Calendar />;
-      case "capacity":
-        return <Capacity />;
-      case "combobox":
-        return <Combobox />;
-      case "description":
-        return <Description />;
-      case "entity":
-        return <Entity />;
-      case "fieldset":
-        return <Fieldset />;
-      case "file-tree":
-        return <FileTree />;
-      case "input":
-        return <Input />;
-      case "keyboard-input":
-        return <KeyboardInput />;
-      case "loading-dots":
-        return <LoadingDots />;
-      case "menu":
-        return <Menu />;
-      case "modal":
-        return <Modal />;
-      case "note":
-        return <Note />;
-      case "radio":
-        return <Radio />;
-      case "select":
-        return <Select />;
-      case "show-more":
-        return <ShowMore />;
-      case "spinner":
-        return <Spinner />;
-      case "stack":
-        return <Stack />;
-      case "status-dot":
-        return <StatusDot />;
-      case "switch":
-        return <Switch />;
-      case "table":
-        return <Table />;
-      case "tabs":
-        return <Tabs />;
-      case "text":
-        return <Text />;
-      case "textarea":
-        return <Textarea />;
-      case "toggle":
-        return <Toggle />;
-      case "tooltip":
-        return <Tooltip />;
-
-      default:
-        return (
-          <div>
-            <h1>🚧 Under construction 🚧</h1>
-            In the process of migrating to the Next.js 13 App Dir.
-          </div>
-        );
-    }
+interface Props extends PropsWithChildren {
+  params: {
+    slug: string;
   };
+}
 
-  return <Page />;
+export async function generateMetadata(
+  { params }: Props,
+  parent?: ResolvingMetadata,
+) {
+  return {
+    title: `${params.slug} | Nextjs Components`,
+  };
+}
+
+export default async function Slug({ params: { slug } }) {
+  return <Renderer slug={slug} />;
 }
