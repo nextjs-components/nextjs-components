f16b44ff636488084a7f3e5b79ba87c61e493158
Kevin Wang <kwangsan@gmail.com>
2023-09-05 18:58:21 -0400

    Badge: v2

--- packages/core/src/components/Badge/Badge.tsx
+++ packages/core/src/components/Badge/Badge.tsx
@@ -1,38 +1,54 @@
 import clsx from "clsx";
-import type { HTMLAttributes } from "react";
+import type { HTMLAttributes, ReactNode } from "react";
 
 import styles from "./Badge.module.css";
 
 interface Props {
   className?: HTMLAttributes<HTMLSpanElement>["className"];
-  type?: "success" | "error" | "warning" | "violet" | "secondary";
-  variant?: "blue" | "red" | "pink" | "amber" | "purple" | "gray" | "blue-subtle" | "red-subtle" | "amber-subtle" | "purple-subtle" | "gray-contrast" | "trial" | "turbo";
-  outline?: boolean;
-  size?: "small" | "large";
+  variant?:
+    | "blue"
+    | "red"
+    | "pink"
+    | "amber"
+    | "purple"
+    | "gray"
+    | "blue-subtle"
+    | "red-subtle"
+    | "amber-subtle"
+    | "purple-subtle"
+    | "gray-contrast"
+    | "trial"
+    | "turbo";
+  size?: "sm" | "md" | "lg";
+  icon?: ReactNode;
 }
 
 const Badge: React.FC<React.PropsWithChildren<Props>> = ({
   children,
   className,
-  type,
   variant,
-  outline,
-  size,
+  size = "md",
+  icon,
 }) => {
   return (
     <span
+      data-geist-badge=""
+      data-version="v2"
       className={clsx(
         styles.badge,
         {
-          [styles[type]]: !!type,
           [styles[variant]]: !!variant,
-          [styles.outline]: outline,
-          [styles[size]]: !!size,
+          [styles.lg]: size === "lg",
+          [styles.md]: size === "md",
+          [styles.sm]: size === "sm",
         },
         className,
       )}
     >
-      {children}
+      <span className={styles.contentContainer}>
+        {icon && <span className={styles.iconContainer}>{icon}</span>}
+        {children}
+      </span>
     </span>
   );
 };
